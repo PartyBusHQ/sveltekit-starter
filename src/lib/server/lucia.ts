158085de0de21df19ae07e54de3c07b9d32f9174
Hunter Stevens <hunter@toplobster.io>
2024-05-02 11:50:56 -0500

    Added server foler, updated module naming conventions, added lucia

---
+++ src/lib/server/lucia.ts
@@ -0,0 +1,7 @@
+import { Lucia } from 'lucia';
+import { PrismaAdapter } from '@lucia-auth/adapter-prisma';
+import { PrismaClient } from '@prisma/client';
+
+const client = new PrismaClient();
+const adapter = new PrismaAdapter(client.session, client.user);
+export const lucia = new Lucia(adapter);
